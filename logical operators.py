b9b4f0ac1597446ad3aa904830b1782e7a1f9f5e
Aravindhkannanb <99198670+Aravindhkannanb@users.noreply.github.com>
2022-02-18 13:43:27 +0530

    Add files via upload

---
+++ logical operators.py
@@ -0,0 +1,35 @@
+#addition
+a = 1234
+b = 214
+print(a+b)
+#substraction
+a =24
+b =234
+print(a-b)
+#multiply
+print(a*b)
+#division
+print(a/b)
+#modulus
+print(a%b)
+
+#Logical operators 
+a = 24
+b =434
+if a and b ==0:
+  print("Yes,that is right")
+else :
+  print("it is not correct")
+
+c = 'apple'
+d =3234
+if c or d =='apple':
+  print('This is correct')
+else:
+  print("wrong")
+
+f =234324234
+if not f==13242134:
+  print(True)
+else:
+  print(False)
